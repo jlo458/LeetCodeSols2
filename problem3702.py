50a504f9481af7c9ea3ab79652c6ae3f16688b81
jlo458 <130149735+jlo458@users.noreply.github.com>
2026-08-15 15:42:08 +0100

    Implement longestSubsequence method in Solution class

---
+++ problem3702.py
@@ -0,0 +1,13 @@
+class Solution:
+    def longestSubsequence(self, nums: List[int]) -> int:
+        total_xor = 0
+        for x in nums:
+            total_xor ^= x
+        
+        if total_xor != 0:
+            return len(nums)
+        
+        if any(x != 0 for x in nums):
+            return len(nums) - 1
+        
+        return 0
